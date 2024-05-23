894c5e5a5aee38cf2fee8a9bc54af1845cf81414
Gemma <jemmimah.abdul@decagon.dev>
2024-05-23 11:52:07 +0100

    SeedCouponTables

--- Mangoro.Services.CouponAPi/Migrations/20240523105024_SeedCouponTables.Designer.cs
+++ Mangoro.Services.CouponAPi/Migrations/20240523105024_SeedCouponTables.Designer.cs
@@ -3,6 +3,7 @@ using Mangoro.Services.CouponAPi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Mangoro.Services.CouponAPi.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240523105024_SeedCouponTables")]
+    partial class SeedCouponTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
