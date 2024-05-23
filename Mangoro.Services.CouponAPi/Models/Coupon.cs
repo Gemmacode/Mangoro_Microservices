3cc40ca258a442eb63c97c0d79f1fb6b97ae79be
Gemma <jemmimah.abdul@decagon.dev>
2024-05-23 11:49:36 +0100

    Created DbContexted

--- Mangoro.Services.CouponAPi/Models/Coupon.cs
+++ Mangoro.Services.CouponAPi/Models/Coupon.cs
@@ -1,7 +1,10 @@
-﻿namespace Mangoro.Services.CouponAPi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mangoro.Services.CouponAPi.Models
 {
     public class Coupon
     {
+        [Required]
         public int CouponId { get; set; } 
         public string CouponCode { get; set; }
         public double DiscountAmount { get; set; } 
